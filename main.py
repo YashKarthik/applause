62d7b30529c5210502b2142ddd2165a55520ed38
Yashashwin Karthikeyan <52369876+YashKarthik@users.noreply.github.com>
2023-05-19 17:48:52 +0530

    squash! Initial commit; figured out bluetoothctl

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import os
 from sh import bluetoothctl
+from gpiozero import LightSensor
 
 PHONE_MAC = os.environ['PHONE_MAC']
+ldr = LightSensor(4)
 
+ldr.wait_for_dark()
+print("Dark")
 bl: str = bluetoothctl("connect", PHONE_MAC)
 success = True if bl.find("uccessful") != -1 else False
-print(success)
+
+# tripwire was broker => door was opened
+# blootooth also connected => I opened the door
+
+if success:
+    print("Me")
