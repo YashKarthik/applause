547bc7e97a3dea91a4eb48d9c7e07994c7b6b651
Yashashwin Karthikeyan <52369876+YashKarthik@users.noreply.github.com>
2023-05-24 13:22:03 +0530

    check LDR in real conditions

--- README.md
+++ README.md
@@ -3,8 +3,11 @@
 Will play "main character music" when I (and only I) walk in through the main door.
 
 - Detect main door state:
-    - Checking door open/close state using LDR (laser tripwires basicall), might use something else
-    later.
+    - ~~Checking door open/close state using LDR (laser tripwires basicall), might use something else
+    later.~~
+    - Using LDR, but not a full-blown laser tripwire circuit. The LDR will be stuck to the top of
+    the door facing the door frame. So when the door is closed, ldr will read 0. When the door is
+    opened, it reads non-zero (ambient light)
 
 - Detect who opened door:
     - When the door is opened, try to connect bluetooth to my phone, if it connects, then I opened the door.
--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 import os
+from time import sleep
 from sh import bluetoothctl
 from gpiozero import LightSensor
 
 PHONE_MAC = os.environ['PHONE_MAC']
 ldr = LightSensor(4)
+tries = 0
 
-ldr.wait_for_dark()
-print("Dark")
-bl: str = bluetoothctl("connect", PHONE_MAC)
-success = True if bl.find("uccessful") != -1 else False
+while True:
+    ldr.wait_for_light()
+    print("\nDoor opened. Detecting who...")
 
-# tripwire was broker => door was opened
-# blootooth also connected => I opened the door
+    try:
+        bl: str = bluetoothctl("connect", PHONE_MAC)
+        success = True if bl.find("uccessful") != -1 else False
 
-if success:
-    print("Me")
+        # blootooth also connected => I opened the door
+        if success:
+            tries = 0
+            print("Yash opened the door")
+            sleep(10) # sleep ten-seconds as I wont re-enter so soon
+    except:
+        # bluetooth could not connect / threw error => I didn't open the door
+        tries += 1
+        print("Stranger")
+
+    if tries > 10:
+        # door may be left open or spam
+        sleep(30)
+
+    if tries > 30:
+        # door definitely left open
+        # shout close the door
+        pass
